d84957792b0a7a962072d5ef19e826eda7d017c1
Matthew Meinzer <meinzer.matthew@gmail.com>
2019-01-02 22:08:38 -0600

    Add cors on server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
 app.disable('x-powered-by');
+app.use(cors());
 
 app.use(bodyParser.json());
 
